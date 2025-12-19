a9ebad156b9854e8e00a50f76ac6920f4001feb7
Alex <alexferanmi390@gmail.com>
2025-12-19 21:03:04 +0100

    chore: pagination wip

--- app/view-wishes/page.tsx
+++ app/view-wishes/page.tsx
@@ -1,20 +1,53 @@
+"use client";
+
+import Loader from "@/component/loader";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
 type wish = {
     id: number;
     name: string;
     message: string;
 };
 
-export default async function ViewWishes() {
-    const response = await fetch(
-        "https://sam-s-birthdayapi-production.up.railway.app/api/v1/birthday-wish"
-    );
-    const json = await response.json();
-    const wishes: wish[] = json.data;
+export default function ViewWishes() {
+    // const response = await fetch(
+    //     "https://sam-s-birthdayapi-production.up.railway.app/api/v1/birthday-wish"
+    // );
+    // const json = await response.json();
+    // const wishes: wish[] = json.data;
+    const [id, setId] = useState<number>(1);
+    const getMessage = async (id: number) => {
+        const response = await fetch(
+            `https://sam-s-birthdayapi-production.up.railway.app/api/v1/birthday-wish?page=${id}`
+        );
+        const json = await response.json();
+        const wishes: wish[] = json.data;
+        return wishes;
+    };
+    const { data, error, isPending, isError } = useQuery({
+        queryKey: ["message", id],
+        queryFn: () => getMessage(id),
+    });
+
     return (
-        <section className="pt-36 pb-10 px-4 min-h-screen xl:px-[220px] bg-[#FCFBFA]">
+        <section className="pt-36 pb-10 px-4 min-h-screen flex flex-col items-center justify-center xl:px-[220px] bg-[#FCFBFA]">
             <div className="container mx-auto">
                 <div className="flex flex-col justify-center sm:flex-row flex-wrap gap-7">
-                    {wishes.map((wish) => (
+                    {isPending && (
+                        <Loader
+                            fill="#6A0DAD"
+                            className="text-[#6A0DAD] w-10 h-10 animate-spin"
+                            role="status"
+                            aria-label="Loading"
+                        />
+                    )}
+                    {isError && (
+                        <span className="text-3xl text-red-500 text-center">
+                            {error.message}
+                        </span>
+                    )}
+                    {data?.map((wish) => (
                         <div key={wish.id} className="w-[302px]">
                             <div>
                                 <div className="h-[235px] drop-shadow-2xl drop-shadow-[#0000000D] px-7 py-7 flex justify-center bg-white rounded-t-2xl rounded-br-2xl custom-clip">
@@ -30,6 +63,18 @@ export default async function ViewWishes() {
                         </div>
                     ))}
                 </div>
+                <section
+                    className={`${
+                        isPending ? "hidden" : "flex"
+                    }  items-center justify-center`}
+                >
+                    <div
+                        onClick={() => setId((prev) => prev + 1)}
+                        className="text-white font-medium text-center cursor-pointer rounded-xl py-3 mt-9 w-fit px-10 bg-[#6A0DAD] text-base"
+                    >
+                        View more
+                    </div>
+                </section>
             </div>
         </section>
     );
--- component/message-wall.tsx
+++ component/message-wall.tsx
@@ -1,51 +1,106 @@
+"use client";
+
 import { ArrowLeft, ArrowRight } from "iconsax-reactjs";
 import Image from "next/image";
 import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
+
+type wish = {
+    id: number;
+    name: string;
+    message: string;
+};
 
 export default function MessageWall() {
+    const getMessage = async () => {
+        const response = await fetch(
+            `https://sam-s-birthdayapi-production.up.railway.app/api/v1/birthday-wish/recent`
+        );
+        const json = await response.json();
+        const wishes: wish[] = json.data;
+        return wishes;
+    };
+    const { data, error } = useQuery({
+        queryKey: ["message"],
+        queryFn: getMessage,
+    });
+
     return (
-        <section className="py-10 md:py-20 px-4 xl:px-[220px]">
-            <div className="container mx-auto">
-                <h1 className="text-black font-medium text-3xl md:text-4xl text-center">
-                    Message wall
-                </h1>
-                <div className="mt-10">
-                    <div className="max-w-[235px]">
-                        <div className="flex items-center gap-2.5">
-                            <Image
-                                src="/svgs/apostrophe.svg"
-                                alt="illustration"
-                                width={34}
-                                height={68}
-                            />
-                            <Image
-                                src="/svgs/apostrophe.svg"
-                                alt="illustration"
-                                width={34}
-                                height={68}
-                            />
+        <section className="py-10 md:py-20">
+            <h1 className="text-black font-medium text-3xl md:text-4xl text-center">
+                Message wall
+            </h1>
+            <div className="mt-10 flex flex-col gap-10 sm:flex-row items-center lg:gap-20 pl-4 xl:pl-[220px]">
+                <div className="max-w-[235px] w-full">
+                    <div className="flex items-center gap-2.5">
+                        <Image
+                            src="/svgs/apostrophe.svg"
+                            alt="illustration"
+                            width={34}
+                            height={68}
+                        />
+                        <Image
+                            src="/svgs/apostrophe.svg"
+                            alt="illustration"
+                            width={34}
+                            height={68}
+                        />
+                    </div>
+                    <h1 className="text-black font-medium mt-9 text-3xl">
+                        Birthday Message for Samuel
+                    </h1>
+                    <div className="mt-10 flex items-center justify-between">
+                        <ArrowLeft
+                            size="32"
+                            color="#0000004D"
+                            className="cursor-pointer"
+                        />
+                        <ArrowRight
+                            size="32"
+                            color="#000000"
+                            className="cursor-pointer"
+                        />
+                    </div>
+                    <Link
+                        href="/view-wishes"
+                        className="text-white font-medium block text-center rounded-xl py-3 mt-9 w-full bg-[#6A0DAD] text-base"
+                    >
+                        View more
+                    </Link>
+                </div>
+                {/* <div className="flex justify-center w-full gap-7 overflow-x-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+                    {data?.map((wish) => (
+                        <div key={wish.id} className="w-[302px] shrink-0">
+                            <div>
+                                <h1 className="text-black">{wish.message}</h1>
+                                <div className="h-[235px] drop-shadow-2xl drop-shadow-[#0000000D] px-7 py-7 flex justify-center bg-white rounded-t-2xl rounded-br-2xl custom-clip">
+                                    <p className="text-black text-sm">
+                                        {wish.message}
+                                    </p>
+                                </div>
+
+                                <h2 className="text-black text-center">
+                                    {wish.name}
+                                </h2>
+                            </div>
                         </div>
-                        <h1 className="text-black font-medium mt-9 text-3xl">
-                            Birthday Message for Samuel
-                        </h1>
-                        <div className="mt-10 flex items-center justify-between">
-                            <ArrowLeft
-                                size="32"
-                                color="#0000004D"
-                                className="cursor-pointer"
-                            />
-                            <ArrowRight
-                                size="32"
-                                color="#000000"
-                                className="cursor-pointer"
-                            />
-                        </div>
-                        <Link
-                            href="/view-wishes"
-                            className="text-white font-medium block text-center rounded-xl py-3 mt-9 w-full bg-[#6A0DAD] text-base"
-                        >
-                            View more
-                        </Link>
+                    ))}
+                </div> */}
+                <div className="w-full overflow-x-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+                    <div className="flex gap-7 w-max px-2 sm:px-0">
+                        {data?.map((wish) => (
+                            <div key={wish.id} className="w-[302px] shrink-0">
+                                <div className="h-[235px] drop-shadow-2xl px-7 py-7 bg-white rounded-t-2xl rounded-br-2xl custom-clip">
+                                    <p className="text-black text-sm">
+                                        {wish.message}
+                                    </p>
+                                </div>
+
+                                <h2 className="text-black text-center mt-2">
+                                    {wish.name}
+                                </h2>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
