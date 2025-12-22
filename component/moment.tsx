26cda83e3bd651862e1bd45fbedf246f6cd10852
Alex <alexferanmi390@gmail.com>
2025-12-22 17:10:37 +0100

    chore: recent endpoint moment

--- component/moment.tsx
+++ component/moment.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
 
 interface sectionProps {
     id: number;
     img: string;
 }
-
+interface moment {
+    id: number;
+    imageUrl: string;
+}
 const thirdSection: sectionProps[] = [
     {
         id: 1,
@@ -34,6 +40,21 @@ const thirdSection: sectionProps[] = [
 ];
 
 export default function Moment() {
+    const getMoment = async () => {
+        const response = await fetch(
+            `https://sam-s-birthdayapi-production.up.railway.app/api/v1/moment/recent`
+        );
+        const json = await response.json();
+        const moment: moment[] = json.data;
+        return moment;
+    };
+    const { data, isPending, isError, isSuccess } = useQuery({
+        queryKey: ["moment"],
+        queryFn: getMoment,
+    });
+
+    console.log(data);
+
     return (
         <section className="pt-10 md:pt-20 px-4 xl:px-[220px]">
             <div className="container mx-auto">
@@ -104,19 +125,26 @@ export default function Moment() {
                         />
                     </div>
                 </div>
-                <div className="grid grid-cols-1 mt-5 gap-5 md:grid-cols-3">
-                    {thirdSection.map((item) => (
-                        <figure key={item.id}>
-                            <Image
-                                className="w-full h-full"
-                                src={item.img}
-                                width={320}
-                                height={360}
-                                alt="sam's Image"
-                            />
-                        </figure>
-                    ))}
-                </div>
+                {isError && (
+                    <p className="text-red-500 mt-4 text-2xl text-center">
+                        Internal server error
+                    </p>
+                )}
+                {isSuccess && (
+                    <div className="grid grid-cols-1 mt-5 gap-5 md:grid-cols-3">
+                        {data?.map((item) => (
+                            <figure key={item.id} className="max-h-[400px]">
+                                <Image
+                                    className="w-full h-full object-cover object-top"
+                                    src={item.imageUrl}
+                                    width={320}
+                                    height={360}
+                                    alt="sam's Image"
+                                />
+                            </figure>
+                        ))}
+                    </div>
+                )}
                 <div className="flex justify-center">
                     <Link
                         href="/view-moments"
